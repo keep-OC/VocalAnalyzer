f39d60a1373f36bdc217afc02679fa301097af72
naca-nyan <naca.nyan@gmail.com>
2025-07-30 01:05:36 +0900

    send as bundle

--- src/osc.rs
+++ src/osc.rs
@@ -1,4 +1,7 @@
 use std::net::UdpSocket;
+use std::time::SystemTime;
+
+use rosc::{encoder, OscBundle, OscMessage, OscPacket, OscType};
 
 const PARAM_FT_L: &str = "/avatar/parameters/FT_L";
 const PARAM_FT_H: &str = "/avatar/parameters/FT_H";
@@ -12,6 +15,12 @@ fn log_scale(x: f32, min: f32, max: f32) -> f32 {
     (x.ln() - min.ln()) / (max.ln() - min.ln())
 }
 
+fn new_float_message(addr: &str, v: f32) -> OscMessage {
+    let mut message = OscMessage::from(addr);
+    message.args.push(OscType::from(v));
+    message
+}
+
 impl OscSender {
     pub fn new() -> Self {
         let host_addr = "127.0.0.1:0";
@@ -20,12 +29,13 @@ impl OscSender {
         sock.connect(to_addr).unwrap();
         Self { sock }
     }
-    fn send_float(&self, addr: &str, v: f32) {
-        let addr = addr.into();
-        let args = vec![rosc::OscType::Float(v)];
-        let message = rosc::OscMessage { addr, args };
-        let packet = rosc::OscPacket::Message(message);
-        let bytes = rosc::encoder::encode(&packet).unwrap();
+    fn send_bundle(&self, vs: Vec<OscMessage>) {
+        let bundle = OscBundle {
+            timetag: SystemTime::now().try_into().unwrap(),
+            content: vs.into_iter().map(|v| OscPacket::Message(v)).collect(),
+        };
+        let packet = OscPacket::Bundle(bundle);
+        let bytes = encoder::encode(&packet).unwrap();
         self.sock.send(&bytes).unwrap();
     }
     pub fn send_frequency(&self, freq: f32) {
@@ -35,8 +45,9 @@ impl OscSender {
         let v = (log_freq * (1u32 << 14) as f32) as u32;
         let ft_l = (v & 0x7F) as f32 / 128.0;
         let ft_h = ((v >> 7) & 0x7F) as f32 / 128.0;
-        self.send_float(PARAM_FT_L, ft_l);
-        self.send_float(PARAM_FT_H, ft_h);
-        self.send_float(PARAM_G1, 1.0);
+        let ft_l = new_float_message(PARAM_FT_L, ft_l);
+        let ft_h = new_float_message(PARAM_FT_H, ft_h);
+        let g1 = new_float_message(PARAM_G1, 1.0);
+        self.send_bundle(vec![ft_l, ft_h, g1]);
     }
 }
